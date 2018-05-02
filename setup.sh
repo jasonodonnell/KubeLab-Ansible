fa6532063dd4b3155c3b100b1e2ecad857c74fa9
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2018-05-02 14:44:22 -0400

    Fix env file

--- setup.sh
+++ setup.sh
@@ -9,8 +9,9 @@ then
 fi
 
 ssh-keygen -b 2048 -t rsa -f ./keys/id_rsa -q -N ""
-chmod 400 ./keys/id_rsa*
 cp ./keys/id_rsa.pub ./roles/common/files/authorized_keys
 cp ./keys/id_rsa ./roles/common/files/id_rsa
+
+chmod 400 ./keys/id_rsa*
 
 exit 0
