aa96b3698cbc1c61b9aecac9107e57ba7d5515ec
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2019-01-24 14:03:50 -0500

    Update kubelab to latest version

--- roles/common/files/env.sh
+++ roles/common/files/env.sh
@@ -4,17 +4,17 @@ export GOPATH=$HOME/cdev
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 export CCP_BASEOS=centos7
-export CCP_PGVERSION=10
-export CCP_PG_FULLVERSION=10.4
-export CCP_VERSION=1.8.3
+export CCP_PGVERSION=11
+export CCP_PG_FULLVERSION=11.1
+export CCP_VERSION=2.3.0
 export CCP_IMAGE_PREFIX=crunchydata
 export CCP_IMAGE_TAG=$CCP_BASEOS-$CCP_PG_FULLVERSION-$CCP_VERSION
 export CCPROOT=$GOPATH/src/github.com/crunchydata/crunchy-containers
 export CCP_CLI=kubectl
 export CCP_NAMESPACE=demo
-export CCP_STORAGE_MODE=ReadWriteMany
+export CCP_STORAGE_MODE=ReadWriteOnce
 export CCP_STORAGE_CAPACITY=400M
-export CCP_STORAGE_CLASS=gluster-heketi
+export CCP_STORAGE_CLASS=hostpath-sc
 export REPLACE_CCP_IMAGE_PREFIX=crunchydata
 
 go get github.com/blang/expenv
